ff2c3cab4e0098c8e14e958b414acb6e080c199d
asimxdev <asimxdev@gmail.com>
2025-05-15 18:55:25 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:musculo_app/core/config/myApp.dart';
 
-
+/////adsasda
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApps());
 }
 
 
